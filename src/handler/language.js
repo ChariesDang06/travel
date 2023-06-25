004c747b55dc51905d29cafb2a321efa1b5e0105
Mucer72 <khanhlsfs@gmail.com>
2023-06-25 09:24:15 +0700

    deploy website

---
+++ src/handler/language.js
@@ -0,0 +1,18 @@
+export const countryList = {
+  vn: "vietnam",
+  en: "american",
+};
+
+export const languageContent = {
+  vn: {
+    contentBanner:
+      "Đồng hành cùng bạn khám phá những trải nghiệm thú vị tại Đà Lạt",
+    buttonContent: "Bắt đầu khám phá",
+    prominentPlace: "Địa Điểm Nổi Bật",
+  },
+  en: {
+    contentBanner: "Be on your side in the adventure of exploring interesting experiences in Dalat",
+    buttonContent: "Start exploring",
+    prominentPlace: "prominentPlace",
+  },
+};
